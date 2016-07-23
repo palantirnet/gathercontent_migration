767197298b0d93ab40dda9a85f69939a5dc5125a
Bec White <white@palantir.net>
2016-07-22 19:56:00 -0500

    Add a process plugin to turn markup containing links into data for a Drupal link field.

---
+++ src/Plugin/migrate/process/GatherContentExtractLinks.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\gathercontent_migration\Plugin\migrate\process\Dump.
+ */
+
+namespace Drupal\gathercontent_migration\Plugin\migrate\process;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+
+/**
+ * @MigrateProcessPlugin(
+ *   id = "gathercontent_extract_links"
+ * )
+ */
+class GatherContentExtractLinks extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $links = array();
+
+    if (preg_match_all('#<a [^>]*href="(.+?)"[^>]*>(.+?)</a>#', $value, $matches, PREG_SET_ORDER)) {
+      foreach ($matches as $m) {
+        $links[] = [
+          'uri' => $m[1],
+          'title' => strip_tags($m[2]),
+          'options' => [],
+        ];
+      }
+    }
+
+    return $links;
+  }
+
+}
